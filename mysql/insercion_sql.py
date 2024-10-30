79d51cfa6551980a1933bfc8d7cb4a9767cf044b
inesposes <inesposes@gmail.com>
2024-10-30 20:29:31 +0100

    Correccións erros na inserción de datos en mysql

--- mysql/insercion_sql.py
+++ mysql/insercion_sql.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import mysql.connector
 from mysql.connector import Error
-
+import os
 # Configurar la conexión a la base de datos
-host = 'localhost'  
-database = 'tu_base_de_datos'
-user = 'tu_usuario'
-password = 'tu_contraseña'
+host = '127.0.0.1'  
+database = 'store'
+user = 'root'
+password = 'changeme'
 
 # Leer el archivo CSV en un DataFrame
-csv_file_path = '../datos.csv'  # Cambia a la ruta de tu archivo CSV
+current_dir = os.getcwd()
+csv_file_path = os.path.abspath(os.path.join(current_dir, 'datos.csv'))
 df = pd.read_csv(csv_file_path)
+
 
 try:
     # Conectarse a la base de datos
@@ -21,21 +23,36 @@ try:
         password=password
     )
 
+
+
     if connection.is_connected():
         cursor = connection.cursor()
+
+        create_table_query = '''
+            CREATE TABLE IF NOT EXISTS users (
+                id INT PRIMARY KEY,
+                name VARCHAR(100),
+                age INT,
+                city VARCHAR(100)
+            )
+            '''
+        cursor.execute(create_table_query)
+
+
         insert_query = '''
-        INSERT INTO tu_tabla (id, name, age, city) VALUES (%s, %s, %s, %s)
+        INSERT INTO users (id, name, age, city) VALUES (%s, %s, %s, %s)
         '''
         for _, row in df.iterrows():
             cursor.execute(insert_query, (int(row['id']), row['name'], int(row['age']), row['city']))
         connection.commit()
         print("Datos insertados con éxito en MySQL")
+        
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Conexión a MySQL cerrada")
+
 
 except Error as e:
     print("Error al conectar a MySQL", e)
 
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("Conexión a MySQL cerrada")
